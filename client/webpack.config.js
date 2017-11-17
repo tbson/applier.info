ed26da4b8811cf9e3c370ffb59793d1af12804e1
Tran Bac Son <tbosn87@gmail.com>
2017-11-17 18:05:17 +0700

    Install flow Setup build

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+const CompressionPlugin = require("compression-webpack-plugin");
 
 const TARGET = process.env.npm_lifecycle_event;
 const path = require('path');
@@ -89,5 +90,59 @@ if(TARGET === 'start' || !TARGET) {
         plugins: [
             new webpack.NamedModulesPlugin()
         ]
+    });
+}
+
+if(['build'].indexOf(TARGET) !== -1) {
+    module.exports = merge(common, {
+        devtool: 'cheap-module-source-map',
+        /*
+        performance: {
+            hints: 'warning', // 'error' or false are valid too
+            maxEntrypointSize: 100000, // in bytes
+            maxAssetSize: 200000 // in bytes
+        },
+        */
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env': {
+                    'NODE_ENV': JSON.stringify('production')
+                }
+            }),
+            // new PrepackWebpackPlugin(configuration),
+            // new BundleAnalyzerPlugin(),
+            new webpack.optimize.UglifyJsPlugin({
+                sourceMap: true,
+                mangle: true,
+                compress: {
+                warnings: false, // Suppress uglification warnings
+                pure_getters: true,
+                unsafe: true,
+                unsafe_comps: true,
+                screw_ie8: true
+                },
+                output: {
+                    comments: false
+                },
+                exclude: [/\.min\.js$/gi] // skip pre-minified libs
+            }),
+            // new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/]),
+            new webpack.NoEmitOnErrorsPlugin(),
+            new CompressionPlugin({
+                asset: "[path].gz[query]",
+                algorithm: "gzip",
+                test: /\.js$|\.css$|\.html$/,
+                threshold: 10240,
+                minRatio: 0
+            })
+            /*
+            new webpack.ProvidePlugin({
+                $: "jquery",
+                jQuery: "jquery",
+                'window.jQuery': 'jquery'
+            })
+            */
+        ],
+        stats: { colors: true }
     });
 }
