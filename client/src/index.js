92b3b6e1893d8a3655f754cb6aadd2372e21f4c7
Tran Bac Son <tbosn87@gmail.com>
2018-03-19 08:05:57 +0700

    remove react-redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router} from 'react-router-dom';
-import {Provider} from 'react-redux';
 import constants from './constants';
 import {History} from 'src/constants';
 import App from './components/App';
